1d6cea18d2c6a732a71416cc4a07d25c58b09bb3
Bas Jansen <bas.jansen@nedap.com>
2011-10-31 12:26:56 +0100

    Changed actions message format

---
+++ csharp/Actions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace StoreIDTopGateAPI
+{
+    [DataContract]
+    class Actions
+    {
+        [DataMember]
+        public Action[] actions { get; set; }
+
+        public Actions(Action[] actions) {
+            this.actions = actions;
+        }
+
+        public Actions(int count) {
+            this.actions = new Action[count];
+        }
+
+        public Action[] getActions() {
+            return this.actions;
+        }
+
+        public void add(int index, Action action) {
+            this.actions[index] = action;
+        }
+    }
+}
